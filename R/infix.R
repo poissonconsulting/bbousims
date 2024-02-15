77f12f3e6767cea3d8584f7eeea0f9335a633761
Seb Dalgarno <seb@poissonconsulting.ca>
2024-02-15 10:14:36 -0800

    add infix operator for matrix multiplication

--- R/infix.R
+++ R/infix.R
@@ -1,12 +1,13 @@
 #' Stochastic matrix multiplication for age-structured population projection.
 #' 
-#' This assumes that matrix x has identical number of rows and columns; matrix y has one column; and matrix y has the same number of rows as matrix x. 
+#' Matrix x should be a process matrix with identical number of rows and columns.
+#' Matrix y should have one column, with each row indicating abundance at a given stage. The number of rows must be identical to the number of columns in x. 
 #' A binomial distribution is used, where size is drawn from the y matrix and prob is drawn from the x matrix. 
 #'  
 #' @param x A matrix with identical number of rows and columns.
-#' @param y A matrix with one column. If vector, it will be converted to a one-column matrix.
+#' @param y A vector or matrix with one column. 
 #'
-#' @return A matrix.
+#' @return A vector of the projected abundance for each stage.
 #' @export
 #'
 #' @examples
@@ -23,14 +24,14 @@
   chk_identical(ncol(x), nrow(y))
   chk_identical(nrow(x), ncol(x))
   
-  res <- matrix(0, ncol = 1, nrow = nrow(x))
+  res <- vector(length = nrow(x))
   
   for(i in 1:nrow(x)){
     bin <- vector(length = nrow(x))
     for(j in 1:length(bin)){
-      bin[j] <- rbinom(1, size = y[i], prob = x[i,j])
+      bin[j] <- rbinom(1, size = y[j,1], prob = x[i,j])
     }
-    res[i,1] <- sum(bin)
+    res[i] <- sum(bin)
   }
   res
 }
