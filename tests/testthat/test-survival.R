51fa157be3e1762670dd94f1e4c9f06f1a462f41
Seb Dalgarno <seb@poissonconsulting.ca>
2024-03-19 15:17:05 -0700

    updated survival functions

--- tests/testthat/test-survival.R
+++ tests/testthat/test-survival.R
@@ -14,12 +14,12 @@ test_that("survival process matrices", {
   expect_identical(dim(x[,,1,1]), c(2L, 2L))
 })
 
-test_that("survival stochastic", {
+test_that("survival stochastic with no random and trend", {
   survival_adult_female <- 0.84
   survival_calf <- 0.5
-  annual_sd <- 0.05
-  period_sd <- 0.05
-  annual_period_sd <- 0.05
+  annual_sd <- 0
+  period_sd <- 0
+  annual_period_sd <- 0
   yearling_effect <- 0.1
   trend <- 0.2
   nyear <- 5
@@ -33,7 +33,32 @@ test_that("survival stochastic", {
                        annual_period_sd = annual_period_sd, nyear = nyear, 
                        nperiod_within_year = nperiod_within_year)
   
+  expect_equal(x[1,1,3], survival_adult_female^(1/12))
+  expect_equal(x[1,1,1], survival_calf^(1/12))
+  expect_equal(x[1,1,2], ilogit(logit(survival_adult_female^(1/12)) + 0.1))
   expect_type(x, "double")
-  expect_identical(dim(x), as.integer(c(nperiod_within_year, nyear, length(stage))))
+  expect_snapshot(x)
+})
+
+test_that("survival stochastic with random and trend", {
+  survival_adult_female <- 0.84
+  survival_calf <- 0.5
+  annual_sd <- 0.1
+  period_sd <- 0.1
+  annual_period_sd <- 0.1
+  yearling_effect <- 0.1
+  trend <- 0.2
+  nyear <- 5
+  nperiod_within_year <- 12
+  
+  x <- survival_period(survival_adult_female = survival_adult_female,
+                       survival_calf = survival_calf,
+                       yearling_effect = yearling_effect,
+                       trend = trend,
+                       annual_sd = annual_sd, period_sd = period_sd,
+                       annual_period_sd = annual_period_sd, nyear = nyear)
+  
+  expect_false(x[1,1,3] == survival_adult_female^(1/12))
+  expect_type(x, "double")
   expect_snapshot(x)
 })
