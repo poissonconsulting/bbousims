a13ef5ee151019c4ec9f2e99ee35a1c1b344afca
Seb Dalgarno <seb@poissonconsulting.ca>
2024-03-29 13:33:01 -0700

    rename age matrix fun

--- tests/testthat/test-age.R
+++ tests/testthat/test-age.R
@@ -1,6 +1,6 @@
 test_that("age process matrix works", {
   age <- c(2, 3, 3)
-  x <- matrix_age(age)
+  x <- bbs_matrix_age(age)
   expect_identical(dim(x), c(3L,3L))
   y <- x %*% c(100, 100, 100)
   expect_identical(y[,1], c(0, 100, 200))
