0c89fd9e7d2c18b7289fad201cc6fc375f14a89c
Seb Dalgarno <seb@poissonconsulting.ca>
2024-04-02 14:48:21 -0700

    add population plot tests

---
+++ tests/testthat/test-plot-population.R
@@ -0,0 +1,29 @@
+test_that("can plot population data.frame", {
+  x <- bbs_simulate_caribou()
+  gp <- bbs_plot_population(x$abundance,)
+  expect_s3_class(gp, "ggplot")
+  expect_snapshot_plot(gp, "population_dataframe")
+})
+
+test_that("can plot population data.frame", {
+  x <- bbs_simulate_caribou()
+  gp <- bbs_plot_population(x$abundance, annual = FALSE)
+  expect_s3_class(gp, "ggplot")
+  expect_snapshot_plot(gp, "population_dataframe_monthly")
+})
+
+test_that("can plot population matrix", {
+  x <- bbs_population_caribou()
+  gp <- bbs_plot_population(x)
+  expect_s3_class(gp, "ggplot")
+  expect_snapshot_plot(gp, "population_matrix")
+})
+
+test_that("can plot population list", {
+  x <- bbs_simulate_caribou()
+  gp <- bbs_plot_population(x)
+  expect_s3_class(gp, "ggplot")
+  expect_snapshot_plot(gp, "population_list")
+})
+
+
