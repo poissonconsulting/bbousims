e24cc14dc91762a6c80042d6ddb1f9ffd1451b95
Seb Dalgarno <seb@poissonconsulting.ca>
2024-04-03 10:57:57 -0700

    fix snapshot name

--- tests/testthat/test-plot-population.R
+++ tests/testthat/test-plot-population.R
@@ -29,6 +29,7 @@ test_that("can plot population matrix 2 stages and 4 periods", {
                            annual_sd = rep(0.05, 2),
                            period_sd = rep(0.1, 2),
                            nyear = 5)
+  set.seed(101)
   fecundity <- bbs_fecundity(intercept = c(NA, logit(0.4)),
                              trend = c(0, -0.2),
                              annual_sd = c(0, 0.1),
@@ -37,13 +38,14 @@ test_that("can plot population matrix 2 stages and 4 periods", {
   birth_mat <- bbs_matrix_birth_year(fecundity, female_recruit_stage = 1, male_recruit_stage = NULL)
   age_mat <- bbs_matrix_age(c(2, 2))
   pop0 <- c(105, 220)
+  set.seed(101)
   x <- bbs_population(pop0, 
                                birth = birth_mat, 
                                age = age_mat, 
                                survival = survival_mat)
   gp <- bbs_plot_population(x)
   expect_s3_class(gp, "ggplot")
-  expect_snapshot_plot(gp, "population_matrix")
+  expect_snapshot_plot(gp, "population_matrix_stage")
 })
 
 test_that("can plot population list", {
