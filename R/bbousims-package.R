026209e11481684d2d1cda5de6da5e3d23e738ed
Joe Thorley <joe@poissonconsulting.ca>
2024-04-30 05:12:12 -0700

    namespace.R to bbousims-package.R

--- R/bbousims-package.R
+++ R/bbousims-package.R
@@ -2,5 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import chk sims magrittr
+#' @importFrom stats rbinom rnorm
+#' @importFrom ggplot2 .data ggplot aes geom_pointrange geom_line geom_ribbon scale_color_manual xlab
+#' @importFrom dplyr mutate arrange group_by slice ungroup select tibble case_when everything all_of
+#' @importFrom tidyr pivot_longer expand_grid
 ## usethis namespace: end
 NULL
--- R/namespace.R
+++
@@ -1,7 +0,0 @@
-#' @import chk sims magrittr
-#' @importFrom stats rbinom rnorm
-#' @importFrom ggplot2 .data ggplot aes geom_pointrange geom_line geom_ribbon scale_color_manual xlab
-#' @importFrom dplyr mutate arrange group_by slice ungroup select tibble case_when everything all_of
-#' @importFrom tidyr pivot_longer expand_grid
-
-NULL
