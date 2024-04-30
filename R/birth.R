cf566dc7ae927e6224187b74771c20c6fb1b0af8
Seb Dalgarno <seb@poissonconsulting.ca>
2024-04-30 12:06:54 -0700

    add alberta license to R scripts, tests, scripts and vignettes

--- R/birth.R
+++ R/birth.R
@@ -1,3 +1,17 @@
+# Copyright 2024 Province of Alberta
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #' Get stochastic fecundity rates by year and stage.
 #'
 #' Year is scaled to Year - 1 for trend, which ensures that the intercept is the rate in the first year.
--- R/devtools-helpers.R
+++ R/devtools-helpers.R
@@ -1,4 +1,4 @@
-# Copyright 2023 Province of Alberta
+# Copyright 2024 Province of Alberta
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
